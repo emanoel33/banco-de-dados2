73a4c2a8813c997431fc92c5024a9c5e43de81c5
Tommx <54634525+emanoel33@users.noreply.github.com>
2024-08-28 10:57:55 -0300

    Add files via upload

---
+++ banco de dados.sql
@@ -0,0 +1,10 @@
+ create database atividade;
+ use atividade;
+ CREATE TABLE folha(
+ id int (50) PRIMARY KEY, 
+ cargo VARCHAR (50),
+ mes int, 
+ Ano year,
+ Nome varchar(255)
+ );
+ select * from folha;
